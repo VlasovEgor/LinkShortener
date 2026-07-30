b672157b58c8ff9c4d1237996336240a201526f2
Egor.Vlasov <qwerty@gmail.com>
2026-07-30 18:28:27 +0300

    update migrations

---
+++ LinkShortener/Migrations/20260730133557_idempotency_key.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LinkShortener.Migrations
+{
+    /// <inheritdoc />
+    public partial class idempotency_key : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IdempotencyKey",
+                table: "ShortLinks",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShortLinks_IdempotencyKey",
+                table: "ShortLinks",
+                column: "IdempotencyKey",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ShortLinks_IdempotencyKey",
+                table: "ShortLinks");
+
+            migrationBuilder.DropColumn(
+                name: "IdempotencyKey",
+                table: "ShortLinks");
+        }
+    }
+}
